23fe3e019f563790a9f6b46db102c8dd6684a72c
Brian Baron <brianbaron@gmail.com>
2017-07-27 13:48:22 -0700

    app.js now adds linebreaks after each 4th game piece.

--- app.js
+++ app.js
@@ -11,6 +11,22 @@ function isInArray(randomArray, num) {
     return false;
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+//// This function formats our game board into a 4X4 grid, thus the integer ////
+//// 4. Function takes the array of already drawn numbers as randomArray    ////
+//// It also takes the jQuery object mainDiv, which points to the main <div>////
+//// tag of the HTML page.                                                  ////
+////////////////////////////////////////////////////////////////////////////////
+function isDivisibleByFour(randomArray, mainDiv) {
+  if (randomArray.length % 4 === 0) {
+    mainDiv.append('<br>');
+  }
+  else {
+    return;
+  }
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 ///Takes in array of game pieces, and jQuery object mainDiv. Populates page ////
 ///with game tokens.                                                        ////
@@ -29,15 +45,29 @@ function generateRandomGameBoard(game_token_array, mainDiv) {
       mainDiv.append(game_token_array[num][1]);
       console.log(game_token_array[num][1]);
       randomArray.push(num);
+      isDivisibleByFour(randomArray, mainDiv);
       console.log(randomArray);
     }
   }
 }
 
 var gameSpaces = [
-              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'square\'><img src=\'\\back.jpg\'></div>'],
-              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'square\'><img src=\'\\back.jpg\'></div>'],
-              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'square\'><img src=\'\\back.jpg\'></div>']
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>'],
+              ['<div class=\'square\'><img src=\'\\face.jpg\'></div>', '<div class=\'squareback\'><img src=\'\\back.jpg\'></div>']
             ];
 //var numberOfGameSpaces = gameSpaces.length;
 
