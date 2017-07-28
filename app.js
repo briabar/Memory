55c601133c06caf4a2950a3c8958f8892a6305e2
Brian Baron <brianbaron@gmail.com>
2017-07-28 11:35:15 -0700

    Add game logic and some styling.

--- app.js
+++ app.js
@@ -51,25 +51,61 @@ function generateRandomGameBoard(game_token_array, mainDiv) {
   }
 }
 
+function animateFade(classTag) {
+  var classTagForAnimation = classTag[2];
+  $('.'+ classTagForAnimation).fadeOut();
+  $('.' + classTagForAnimation).fadeIn();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//// Function is responsible for game onclick events and gamelogic.         ////
+////////////////////////////////////////////////////////////////////////////////
+function gameLogic() {
+  $('.front').click(function() {
+    $(this).addClass('vanish');
+    answers.push($(this).attr('class'));
+    clicks += 1
+    if (clicks === 2) {
+      clicks = 0;
+      if (answers[0] === answers[1]) {
+        var classTag = answers[0].split(" ");
+        $('.' + classTag[0]).addClass('pairfound');
+        answers = [];
+      }
+      else {
+        var classTag = answers[0].split(" ");
+        var classTag2 = answers[1].split(" ");
+        $('.' + classTag).removeClass('vanish');
+        animateFade(classTag2);
+        $('.' + classTag2).removeClass('vanish');
+        answers = [];
+      }
+    }
+  })
+}
+
 var gameSpaces = [
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>'],
-              ['<div class=\'square\'><img class=\'front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back.jpg\'></div>']
+              ['<div class=\'square\'><img class=\'ONE front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back1.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'ONE front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back1.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'TWO front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back2.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'TWO front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back2.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'THREE front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back3.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'THREE front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back3.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'FOUR front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back4.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'FOUR front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back4.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'FIVE front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back5.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'FIVE front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back5.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'SIX front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back6.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'SIX front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back6.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'SEVEN front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back7.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'SEVEN front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back7.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'EIGHT front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back8.jpg\'></div>'],
+              ['<div class=\'square\'><img class=\'EIGHT front\' src=\'face.jpg\'></div>', '<div class=\'squareback\'><img class=\'back\' src=\'back8.jpg\'></div>']
             ];
 //var numberOfGameSpaces = gameSpaces.length;
 
-var mainDiv = $('.gamemain');
-generateRandomGameBoard(gameSpaces, mainDiv);
+var $mainDiv = $('.gamemain');
+var clicks = 0;
+var answers = [];
+generateRandomGameBoard(gameSpaces, $mainDiv);
+gameLogic(clicks, answers);
